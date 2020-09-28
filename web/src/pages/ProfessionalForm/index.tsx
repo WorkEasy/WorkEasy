91ba7076ac67fc24ed8635636faa29752baa6d2b
landin <7barganha@gmail.com>
2020-09-28 20:47:32 -0300

    Update: styles.css and index.tsx | Add: warning.svg

--- web/src/pages/ProfessionalForm/index.tsx
+++ web/src/pages/ProfessionalForm/index.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import Input from '../../components/input';
 import PageHeader from '../../components/PageHeader';
+import './styles.css';
+
+import warningIcon from '../../assets/images/icons/icons8-warning.svg'
 
 function ProfessionalForm(){
     return(
         <div id="page-professional-form" className="container">
-            <PageHeader title="Que incrivel, você resolve problemas !"/>
+            <PageHeader 
+            title="Que incrivel, você resolve problemas !"
+            description="Primeiro passo é preencher esse formulário de inscrição."
+            />
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input name="name" label="Nome Completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="whatsapp" label="WhatsApp"/>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Input name="tipServico" label="Serviço"/>
+                    <Input name="custo" label="Custo da sua hora serviço"/>
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="aviso"/>
+                        Aviso Importante <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+            </main>
         </div>  
     )
 }
--- web/src/pages/ProfessionalsList/index.tsx
+++ web/src/pages/ProfessionalsList/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Input from '../../components/input';
 import PageHeader from '../../components/PageHeader';
 import ProfisionalItem from '../../components/ProfessionalItem';
 
@@ -11,19 +12,10 @@ function ProfessionalList(){
          <div id="page-professional-list" className="container">
             <PageHeader title="Profissionais disponíveis">
                 <form id="search-professionals">
-                    <div className="input-block">
-                        <label htmlFor="subject">Profissões</label>
-                        <input type="text" id="subject"/>
-                    </div>
-                    
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dia da semana</label>
-                        <input type="text" id="week-day"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Cidade</label>
-                        <input type="text" id="time"/>
-                    </div>
+                    <Input name="tipServico" label="Profissão"/>
+                    <Input name="week_day" label="Dia da semana"/>
+                    <Input type="time" name="time" label="Hora"/>
+
 
                 </form>
             </PageHeader>
