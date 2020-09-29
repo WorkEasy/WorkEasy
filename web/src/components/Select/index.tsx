8016bcc47a80d7c9a0f519238abb4049487d21b4
landin <7barganha@gmail.com>
2020-09-29 01:10:53 -0300

    🔥-update folder: campo select com opçoes > ProfessionalsList

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -16,8 +16,9 @@ const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
             <select id={name} {...rest}>
+                <option value="" disabled selected hidden>Selecione uma opção</option>
                 {options.map(option => {
-                    return <option value={option.value}>{option.label}</option>
+                    return <option key={option.value} value={option.value}>{option.label}</option>
                 })}
             </select>
         </div>
--- web/src/pages/ProfessionalsList/index.tsx
+++ web/src/pages/ProfessionalsList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import ProfisionalItem from '../../components/ProfessionalItem';
+import Select from '../../components/Select';
 
 
 import './styles.css';
@@ -12,8 +13,32 @@ function ProfessionalList(){
          <div id="page-professional-list" className="container">
             <PageHeader title="Profissionais disponíveis">
                 <form id="search-professionals">
-                    <Input name="tipServico" label="Profissão"/>
-                    <Input name="week_day" label="Dia da semana"/>
+                <Select 
+                        name="tipServico"
+                        label="Serviço"
+                        options={[
+                            { value: 'Predeiro', label: 'Predeiro' },
+                            { value: 'Pintor', label: 'Pintor' },
+                            { value: 'Marceneiro', label: 'Marceneiro' },
+                            { value: 'Eletricista', label: 'Eletricista' },
+                            { value: 'Encanador', label: 'Encanador' },
+                            { value: 'Jardineiro', label: 'Jardineiro' },
+                            { value: 'Suporte técnico', label: 'Suporte técnico' },
+                        ]}
+                    />
+                    <Select 
+                        name="week_day"
+                        label="Dia da semana"
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' },
+                        ]}
+                    />
                     <Input type="time" name="time" label="Hora"/>
 
 
